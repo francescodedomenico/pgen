b9981bd8c07349d4441f1e41197fb0a4c3690868
Francesco De Domenico <dedomenico.francesco@outlook.com>
2022-09-23 14:41:56 +0200

    aggiunta parametro n

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use clap::Parser;
-use rand::Rng;
-#[derive(Parser,Default,Debug)]
-#[clap(author="Francesco De Domenico", version, about="A Very simple Password Generator!")]
+use rand::{rngs::ThreadRng, Rng};
+#[derive(Parser, Default, Debug)]
+#[clap(
+    author = "Francesco De Domenico",
+    version,
+    about = "A Very simple Password Generator!"
+)]
 struct Arguments {
     /// The desided length for the password
     #[clap(short, long, value_parser, value_name = "LENGTH")]
@@ -11,27 +15,28 @@ struct Arguments {
     complexity: Option<u8>,
     /// A custom dictionary for the password, if set the complexity parameter is ignored.
     #[clap(short, long, value_parser, value_name = "DICTIONARY")]
-    dictionary: Option<String>
+    dictionary: Option<String>,
+
+    /// Number of passwords to be generated
+    #[clap(short, long, value_parser, value_name = "NUMBER")]
+    number: Option<u64>,
 }
 
-fn main() {
-    let args = Arguments::parse();
-
+fn make_dictionary(args: &Arguments) -> String {
     const UPPERCASE: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const LOWERCASE: &str = "abcdefghijklmnopqrstuvwxyz";
     const NUMBERS: &str = "0123456789";
     const SYMBOLS: &str = "\\|\"$%&/()='?^~";
     let mut dictionary: String = "".to_string();
-    
-    if let Some(input_dictionary) = args.dictionary.as_deref(){
+
+    if let Some(input_dictionary) = args.dictionary.as_deref() {
         dictionary = input_dictionary.to_string();
-    }
-    else{
-        if let Some(input_complexity) = args.complexity{
-            match input_complexity{
+    } else {
+        if let Some(input_complexity) = args.complexity {
+            match input_complexity {
                 1 => {
                     dictionary.push_str(&LOWERCASE);
-                },
+                }
                 2 => {
                     dictionary.push_str(&UPPERCASE);
                     dictionary.push_str(&LOWERCASE);
@@ -47,27 +52,40 @@ fn main() {
                     dictionary.push_str(&NUMBERS);
                     dictionary.push_str(&SYMBOLS);
                 }
-                _ =>{}
+                _ => {}
             }
-        }
-        else{
+        } else {
             dictionary.push_str(&UPPERCASE);
             dictionary.push_str(&LOWERCASE);
             dictionary.push_str(&NUMBERS);
             dictionary.push_str(&SYMBOLS);
         }
-        
     }
+    dictionary
+}
 
-    
-
-    let mut rng = rand::thread_rng();
-
+fn make_password(args: &Arguments, dictionary: &String, rng: &mut ThreadRng) -> String {
     let password: String = (0..args.length)
         .map(|_| {
             let idx = rng.gen_range(0..dictionary.len());
             dictionary.chars().nth(idx).unwrap() as char
         })
         .collect();
-    println!("{}", password);
+    password
+}
+fn main() {
+    let args = Arguments::parse();
+    let dictionary: String = make_dictionary(&args);
+    let mut rng = rand::thread_rng();
+    println!("\n");
+    if let Some(input_number) = args.number {
+        for i in 0..input_number {
+            let password: String = make_password(&args, &dictionary, &mut rng);
+            println!("{}: {}", i, password);
+            println!("\n");
+        }
+    } else {
+        let password: String = make_password(&args, &dictionary, &mut rng);
+        println!("{}", password);
+    }
 }
